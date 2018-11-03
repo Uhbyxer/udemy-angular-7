3d288d4718dd5de9cd198a6c9a930d38d10a9120
Taras Hrynchuk <Taras_Hrynchuk@epam.com>
2018-11-03 15:42:30 +0200

    add recipe service

--- recipe/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
+import {RecipeService} from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,19 +9,12 @@ import {Recipe} from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[];
 
-  recipes: Recipe[] = [
-    new Recipe('Rise', 'Omnomnom', 'https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fimg1.southernliving.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2F2017%2F12%2Fmain%2F2548201_lostc_0009.jpg%3Fitok%3DEOonZOIk&w=700&q=85')
-    ,
-    new Recipe('Pizzka', 'mmmmmm', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg')
-  ];
-
-  constructor() { }
+  constructor(private recipeService: RecipeService) {}
 
   ngOnInit() {
+    this.recipes = this.recipeService.recipes;
   }
 
-  onRecipeSelected(rec: Recipe) {
-    this.recipeWasSelected.emit(rec);
-  }
 }
