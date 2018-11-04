120c6710ac12cab3588e05ab8f9b36c65d7d9010
Taras Hrynchuk <Taras_Hrynchuk@epam.com>
2018-11-05 01:27:04 +0200

    observable: interval of numbers

--- observables-start/src/app/home/home.component.ts
+++ observables-start/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs';
+import 'rxjs-compat/add/observable/interval';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit() {
+    const myNumbers = Observable.interval(1000);
+
+    myNumbers.subscribe((number: number) => {
+      console.log(number);
+    });
   }
 
 }
