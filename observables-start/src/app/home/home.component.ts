fbc22d90299516a18c877ecb6a9985e436140ba4
Taras Hrynchuk <Taras_Hrynchuk@epam.com>
2018-11-05 01:57:11 +0200

    observable: custom observer

--- observables-start/src/app/home/home.component.ts
+++ observables-start/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Observable} from 'rxjs';
+import {Observable, Observer} from 'rxjs';
 import 'rxjs-compat/add/observable/interval';
 
 @Component({
@@ -13,11 +13,39 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const myNumbers = Observable.interval(1000);
+    // const myNumbers = Observable.interval(1000);
+    //
+    // myNumbers.subscribe((number: number) => {
+    //   console.log(number);
+    // });
 
-    myNumbers.subscribe((number: number) => {
-      console.log(number);
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first package');
+      }, 2000);
+
+      setTimeout(() => {
+        observer.next('second package');
+      }, 4000);
+
+      setTimeout(() => {
+        observer.error('Does not work');
+      }, 5000);
+
+      setTimeout(() => {
+        observer.complete();
+      }, 6000);
+
+      setTimeout(() => {
+        observer.next('third package');
+      }, 7000);
     });
+
+    myObservable.subscribe(
+      (data: string) => {console.log(data); },
+      (error: string) => {console.log(error); },
+      () => {console.log('completed'); }
+      );
   }
 
 }
